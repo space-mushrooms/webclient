0d912ef027c0aa53f0aadafab2c63b4bcf7e8511
Max Nikitin <maxnik4u@gmail.com>
2018-10-20 19:40:14 +0300

    add video

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,16 +13,21 @@ export default function Card({ children, backgroundImage, height, shadow }) {
           height,
         }}
       >
+        <video
+          className={cn('video')}
+          src="https://coubsecure-s.akamaihd.net/get/b151/p/coub/simple/cw_file/9797a93278f/1b689ed594b9b17f88b2e/gifv_1539470990_5362149_gifv.mp4"
+          autoPlay
+          loop
+          muted
+          controls={false}
+          height="100%"
+          width="100%"
+        />
         {shadow ? (
-          <div
-            className={cn('overlay', 'root')}
-          >
+          <div className={cn('overlay', 'root')}>
             {children}
           </div>
-        ) : (
-            children
-          )
-        }
+        ) : children}
       </div>
   )
 }
