d5d5f2967ba55a6e4bf4219e80bfed583b9bb18b
Daniil Yastremskiy <catharsis@post.cz>
2018-10-20 20:13:20 +0300

    Launch link

--- src/components/LaunchBanner/index.js
+++ src/components/LaunchBanner/index.js
@@ -3,28 +3,34 @@ import React from 'react';
 import styles from './index.module.css';
 import Card from '../Card';
 import LaunchInfo from '../LaunchInfo';
+import { Link } from 'react-router';
+import format from 'date-fns/format'
 
 const cn = classnames.bind(styles);
 
 const TIMER_DATE_THRESHOLD = 1000 * 60 * 60 * 24 * 5; // 5 days
 
 export default function LaunchBanner({ backgroundImage, mission, rocket, launchTime, live }) {
-  const now = Date.now();
-  const shouldDisplayDate = (launchTime + TIMER_DATE_THRESHOLD) > now;
-  const rightNow = true//live && launchTime < now;
+  const now = new Date();
+  const shouldDisplayDate = (launchTime - TIMER_DATE_THRESHOLD) > now;
+  const rightNow = live && launchTime < now;
 
   return (
-    <Card backgroundImage={backgroundImage} shadow height="300px">
-      <div>
-        <LaunchInfo title="Mission" text={mission} />
-        <LaunchInfo title="Rocket" text={rocket} />
-      </div>
-      <div className={cn('launchTimer')}>
-        {rightNow && 'Right now'}
-      </div>
-      <button className={cn('exporeButton')}>
-        Expore now
-      </button>
-    </Card>
+    <Link to="/" className={cn('root')}>
+      <Card backgroundImage={backgroundImage} shadow height="390px">
+        {live && <span className={cn('live')}>LIVE</span>}
+        <div>
+          <LaunchInfo title="Mission" text={mission} />
+          <LaunchInfo title="Rocket" text={rocket} />
+        </div>
+        <div className={cn('launchTimer')}>
+          {rightNow && 'Right now'}
+          {shouldDisplayDate && format(now, 'MM/DD/YY')}
+        </div>
+        <div className={cn('explore')}>
+          Explore now
+        </div>
+      </Card>
+    </Link>
   );
 }
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import Swipe from '../../components/Swipe';
 import LaunchBanner from '../../components/LaunchBanner';
 import Sliding from '../../components/PageTransition/Sliding';
@@ -14,16 +14,20 @@ export default class MainPage extends PureComponent {
               backgroundImage="https://images-assets.nasa.gov/image/201304210010HQ/201304210010HQ~medium.jpg"
               mission="Commercial Crew SpaceX"
               rocket="SpaceX Falcon Heavy"
+              launchTime={Date.now() - 1000 * 60 * 10}
+              live
             />
             <LaunchBanner
               backgroundImage="https://images-assets.nasa.gov/image/201304210010HQ/201304210010HQ~medium.jpg"
               mission="Commercial Crew SpaceX"
               rocket="SpaceX Falcon Heavy"
+              launchTime={Date.now() + 1000 * 60 * 60 * 24}
             />
             <LaunchBanner
               backgroundImage="https://images-assets.nasa.gov/image/201304210010HQ/201304210010HQ~medium.jpg"
               mission="Commercial Crew SpaceX"
               rocket="SpaceX Falcon Heavy"
+              launchTime={Date.now() + 1000 * 60 * 60 * 24 * 10}
             />
           </Swipe>
         </TitledSection>
