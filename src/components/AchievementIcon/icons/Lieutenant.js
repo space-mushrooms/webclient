d7c69b4cec2bf5c127503d0e1036c41bfb31fcd1
pavelglushkov <pashutk@joom.it>
2018-10-20 17:27:57 +0300

    AchievementIcon component

---
+++ src/components/AchievementIcon/icons/Lieutenant.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ({fill = '#E26119'}) => (
+  <svg width="8" height="21" viewBox="0 0 8 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 20.5V0.5C0 0.223858 0.223858 0 0.5 0H7.5C7.77614 0 8 0.223858 8 0.500001V20.5C8 20.7761 7.77614 21 7.5 21H0.5C0.223858 21 0 20.7761 0 20.5Z" fill={fill}/>
+  </svg>
+);
