14f939386410052afa40664af43f986245b09e1b
Daniil Yastremskiy <catharsis@post.cz>
2018-10-20 13:32:31 +0300

    Router

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './reducers'
+import {Router, Route, browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
-    <App />
+  <Provider store={store}>
+  <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
   </Provider>,
 document.getElementById('root'));
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux'
+
 import mission from './mission';
 
 export default combineReducers({
     mission,
+    routing: routerReducer,
 });
